63233f9890931ecd8c36a9c8e23198e1e93cf10f
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-09-17 20:35:49 +0530

    tweaks

--- components/blockFeed.tsx
+++ components/blockFeed.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Button } from "@/components/ui/button";
